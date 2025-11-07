8d8e2d158af7a1155103c147c2e6affeffe753bf
Goober5000 <ipw47@mailbox.org>
2025-11-06 23:34:59 -0500

    refactor ship anchors
    Testing in FotG exposed a bug that dates back to retail.  During mission parsing, arrival and departure anchors were represented by indexes into `Parse_names`.  After parsing, they were converted to ship indexes -- but only in FRED.  Since some functions are shared between FRED and FS2, this can produce inconsistent results, which can be hard to detect since parse indexes and ship indexes are usually the same.
    
    This refactors arrival and departure anchors to be represented by ship registry indexes in all cases.  It also adds conversion functions for the benefit of FRED and QtFRED since those dialogs were designed with ship indexes in mind.

--- code/missioneditor/missionsave.cpp
+++ code/missioneditor/missionsave.cpp
@@ -3562,22 +3562,18 @@ int Fred_mission_save::save_objects()
 				// save it
 				fout(" %s", tmp);
 			} else {
-				fout(" %s", Ships[z].ship_name);
+				fout(" %s", ship_registry_get(z)->name);
 			}
 		}
 
 		// Goober5000
 		if (save_config.save_format != MissionFormat::RETAIL) {
 			if ((shipp->arrival_location == ArrivalLocation::FROM_DOCK_BAY) && (shipp->arrival_path_mask > 0)) {
-				int anchor_shipnum;
-				polymodel* pm;
-
-				anchor_shipnum = shipp->arrival_anchor;
-				Assert(anchor_shipnum >= 0 && anchor_shipnum < MAX_SHIPS);
+				auto anchor_entry = ship_registry_get(shipp->arrival_anchor);
+				auto pm = model_get(anchor_entry->sip()->model_num);
 
 				fout("\n+Arrival Paths: ( ");
 
-				pm = model_get(Ship_info[Ships[anchor_shipnum].ship_info_index].model_num);
 				for (auto n = 0; n < pm->ship_bay->num_paths; n++) {
 					if (shipp->arrival_path_mask & (1 << n)) {
 						fout("\"%s\" ", pm->paths[pm->ship_bay->path_indexes[n]].name);
@@ -3615,7 +3611,7 @@ int Fred_mission_save::save_objects()
 			parse_comments();
 
 			if (shipp->departure_anchor >= 0)
-				fout(" %s", Ships[shipp->departure_anchor].ship_name);
+				fout(" %s", ship_registry_get(shipp->departure_anchor)->name);
 			else
 				fout(" <error>");
 		}
@@ -3623,15 +3619,11 @@ int Fred_mission_save::save_objects()
 		// Goober5000
 		if (save_config.save_format != MissionFormat::RETAIL) {
 			if ((shipp->departure_location == DepartureLocation::TO_DOCK_BAY) && (shipp->departure_path_mask > 0)) {
-				int anchor_shipnum;
-				polymodel* pm;
-
-				anchor_shipnum = shipp->departure_anchor;
-				Assert(anchor_shipnum >= 0 && anchor_shipnum < MAX_SHIPS);
+				auto anchor_entry = ship_registry_get(shipp->departure_anchor);
+				auto pm = model_get(anchor_entry->sip()->model_num);
 
 				fout("\n+Departure Paths: ( ");
 
-				pm = model_get(Ship_info[Ships[anchor_shipnum].ship_info_index].model_num);
 				for (auto n = 0; n < pm->ship_bay->num_paths; n++) {
 					if (shipp->departure_path_mask & (1 << n)) {
 						fout("\"%s\" ", pm->paths[pm->ship_bay->path_indexes[n]].name);
@@ -4940,22 +4932,18 @@ int Fred_mission_save::save_wings()
 				// save it
 				fout(" %s", tmp);
 			} else {
-				fout(" %s", Ships[z].ship_name);
+				fout(" %s", ship_registry_get(z)->name);
 			}
 		}
 
 		// Goober5000
 		if (save_config.save_format != MissionFormat::RETAIL) {
 			if ((w.arrival_location == ArrivalLocation::FROM_DOCK_BAY) && (w.arrival_path_mask > 0)) {
-				int anchor_shipnum;
-				polymodel* pm;
-
-				anchor_shipnum = w.arrival_anchor;
-				Assert(anchor_shipnum >= 0 && anchor_shipnum < MAX_SHIPS);
+				auto anchor_entry = ship_registry_get(w.arrival_anchor);
+				auto pm = model_get(anchor_entry->sip()->model_num);
 
 				fout("\n+Arrival Paths: ( ");
 
-				pm = model_get(Ship_info[Ships[anchor_shipnum].ship_info_index].model_num);
 				for (auto n = 0; n < pm->ship_bay->num_paths; n++) {
 					if (w.arrival_path_mask & (1 << n)) {
 						fout("\"%s\" ", pm->paths[pm->ship_bay->path_indexes[n]].name);
@@ -4989,7 +4977,7 @@ int Fred_mission_save::save_wings()
 			parse_comments();
 
 			if (w.departure_anchor >= 0)
-				fout(" %s", Ships[w.departure_anchor].ship_name);
+				fout(" %s", ship_registry_get(w.departure_anchor)->name);
 			else
 				fout(" <error>");
 		}
@@ -4997,15 +4985,11 @@ int Fred_mission_save::save_wings()
 		// Goober5000
 		if (save_config.save_format != MissionFormat::RETAIL) {
 			if ((w.departure_location == DepartureLocation::TO_DOCK_BAY) && (w.departure_path_mask > 0)) {
-				int anchor_shipnum;
-				polymodel* pm;
-
-				anchor_shipnum = w.departure_anchor;
-				Assert(anchor_shipnum >= 0 && anchor_shipnum < MAX_SHIPS);
+				auto anchor_entry = ship_registry_get(w.departure_anchor);
+				auto pm = model_get(anchor_entry->sip()->model_num);
 
 				fout("\n+Departure Paths: ( ");
 
-				pm = model_get(Ship_info[Ships[anchor_shipnum].ship_info_index].model_num);
 				for (auto n = 0; n < pm->ship_bay->num_paths; n++) {
 					if (w.departure_path_mask & (1 << n)) {
 						fout("\"%s\" ", pm->paths[pm->ship_bay->path_indexes[n]].name);
