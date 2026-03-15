a2321b1f1dbf88331c7ea3f2e75ffc1c32856d4d
Goober5000 <ipw47@mailbox.org>
2026-03-15 04:08:11 -0400

    modify initialization gate

--- fred2/mcp_reference_tools.h
+++ fred2/mcp_reference_tools.h
@@ -2,11 +2,6 @@
 #define _MCP_REFERENCE_TOOLS_H
 
 #include <jansson.h>
-#include <atomic>
-
-// Flag indicating that game tables have been fully loaded and are safe to query.
-// Set by fred_init() after all table parsing is complete.
-extern std::atomic<bool> mcp_tables_ready;
 
 // Append reference/discovery tool schemas to the tools array used by tools/list.
 void mcp_register_reference_tools(json_t *tools);
