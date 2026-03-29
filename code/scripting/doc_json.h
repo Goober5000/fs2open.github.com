6a3621baf2e714e5354b224c578cc3fc47fab62a
Goober5000 <ipw47@mailbox.org>
2026-03-28 21:22:02 -0400

    scripting json

--- code/scripting/doc_json.h
+++ code/scripting/doc_json.h
@@ -2,8 +2,14 @@
 
 #include "scripting_doc.h"
 
+struct json_t;
+
 namespace scripting {
 
 void output_json_doc(const ScriptingDocumentation& doc, const SCP_string& filename);
+
+// Build the scripting API documentation as a jansson JSON tree.
+// Caller takes ownership and must call json_decref() when done.
+json_t* build_json_doc(const ScriptingDocumentation& doc);
 
 }
